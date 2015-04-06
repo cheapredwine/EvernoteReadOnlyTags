cf863b3049fa61008a2e06ef277fededfcc297e8
cheapredwine <jason@sherron.com>
2015-04-06 10:30:19 -0700

    initial commit

---
+++ EvernoteReadOnlyTags/EvernoteConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EvernoteSDK;
+using EvernoteSDK.Advanced;
+using Evernote.EDAM.Type;
+
+namespace EvernoteReadOnlyTags
+{
+    class EvernoteConnection
+    {
+        private static ENSession session;
+
+        public static void Create()
+        {
+            const string DEV_TOKEN = @"S=s1:U=90a30:E=153e3c3aab4:C=14c8c127c60:P=1cd:A=en-devtoken:V=2:H=d8d13e0d201c83db041872b2f5a9f691";
+
+            string url = System.Configuration.ConfigurationManager.AppSettings["URL"];
+
+            ENSessionAdvanced.SetSharedSessionDeveloperToken(DEV_TOKEN, url);
+            if (ENSession.SharedSession.IsAuthenticated == false)
+            {
+                ENSession.SharedSession.AuthenticateToEvernote();
+            }
+
+            session = ENSession.SharedSession;
+
+            EvernoteReadOnlyTagsException.Assert(ENSession.SharedSession.IsAuthenticated, "Authentication failed");
+        }
+
+
+        public static ENSession CurrentSession
+        {
+            get
+            {
+                EvernoteReadOnlyTagsException.Assert(ENSession.SharedSession.IsAuthenticated, "Not connected");
+                return session;
+            }
+        }
+    }
+}
---
+++ EvernoteReadOnlyTags/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using EvernoteSDK;
+using EvernoteSDK.Advanced;
+using Evernote.EDAM.Type;
+
+namespace EvernoteReadOnlyTags
+{
+    class Program
+    {
+        public const string READONLY_CONTENT_CLASS = @"jasonsherron.readonly";
+        public const string READONLY_TAG = @"ReadOnly";
+
+        static void Main(string[] args)
+        {
+            EvernoteConnection.Create();
+            
+            //SampleData.CreateSampleNote(true, READONLY_TAG);
+
+            // lock newly-tagged notes
+            SetTaggedNotesToReadOnly();
+
+            // unset - let user remove tags to reset notes to r/w
+            InteractiveRemoveReadOnlyTags();
+
+            // sweep and reset to read-write
+            ResetUntaggedNotesToReadWrite();
+        }
+
+        private static void InteractiveRemoveReadOnlyTags()
+        {
+            const string READONLY_TAG_SEARCH_QUERY = @"tag:" + READONLY_TAG;
+
+            List<ENSessionFindNotesResult> readOnlyNotes = FindNotes(READONLY_TAG_SEARCH_QUERY);
+
+            Console.WriteLine("all readonly notes: " + readOnlyNotes.Count);
+            Debug.WriteLine("all readonly notes: " + readOnlyNotes.Count);
+
+            foreach(ENSessionFindNotesResult readOnlyNote in readOnlyNotes)
+            {
+                Console.WriteLine(string.Format("Untag '{0}'?", readOnlyNote.Title));
+                Console.Write("Y / N: ");
+
+                if ("Y" == Console.ReadLine().ToUpper())
+                {
+                    ENNoteRef noteRef = readOnlyNote.NoteRef;
+                    ENNoteStoreClient noteStore = ENSessionAdvanced.SharedSession.NoteStoreForNoteRef(noteRef);
+                    List<string> tagGuids = noteStore.GetNoteTagNames(noteRef.Guid);
+                    List<Tag> tags = noteStore.ListTags();
+                    Tag readOnlyTag = tags.Find(x => x.Name.Contains(READONLY_TAG));
+                    Note loadedNote = noteStore.GetNote(noteRef.Guid, true, false, false, false);
+
+                    loadedNote.TagGuids.Remove(readOnlyTag.Guid);
+                    noteStore.UpdateNote(loadedNote);
+                }
+            }
+        }
+
+        private static void SetTaggedNotesToReadOnly()
+        {
+            // find notes with readonly tag but no contentClass
+            const string READONLY_TAG_NO_CONTENTCLASS_SEARCH = @"tag:" + READONLY_TAG + " -contentClass:" + READONLY_CONTENT_CLASS;
+
+            List<ENSessionFindNotesResult> notesToSetReadOnly = FindNotes(READONLY_TAG_NO_CONTENTCLASS_SEARCH);
+
+            Console.WriteLine("newly-tagged notes to mark readonly: " + notesToSetReadOnly.Count);
+            Debug.WriteLine("newly-tagged notes to mark readonly: " + notesToSetReadOnly.Count);
+
+            // add r/o contentClass to every found note (with r/o tag)
+            foreach (ENSessionFindNotesResult noteToSetReadOnly in notesToSetReadOnly)
+            {
+                ENNoteRef noteRef = noteToSetReadOnly.NoteRef;
+                ENNoteStoreClient noteStore = ENSessionAdvanced.SharedSession.NoteStoreForNoteRef(noteRef);
+                Note loadedNote = noteStore.GetNote(noteRef.Guid, true, false, false, false);
+                if (null == loadedNote.Attributes.ContentClass)
+                {
+                    loadedNote.Attributes.ContentClass = READONLY_CONTENT_CLASS;
+                    noteStore.UpdateNote(loadedNote);
+                }
+            }
+        }
+
+        private static void ResetUntaggedNotesToReadWrite()
+        {
+            // find notes with the contentClass but no tag
+            const string READONLY_NOTAG_CONTENT_CLASS_SEARCH = @"contentClass:" + READONLY_CONTENT_CLASS + " -tag:" + READONLY_TAG;
+
+            List<ENSessionFindNotesResult> untaggedNotes = FindNotes(READONLY_NOTAG_CONTENT_CLASS_SEARCH);
+
+            Console.WriteLine("untagged notes to reset to r/w: " + untaggedNotes.Count);
+            Debug.WriteLine("untagged notes to reset to r/w: " + untaggedNotes.Count);
+
+            // Clear the contentClass for anything without the ReadOnly tag
+            foreach (ENSessionFindNotesResult noteToSetReadWrite in untaggedNotes)
+            {
+                ENNoteRef noteRef = noteToSetReadWrite.NoteRef;
+                ENNoteStoreClient noteStore = ENSessionAdvanced.SharedSession.NoteStoreForNoteRef(noteRef);
+                List<string> tags = noteStore.GetNoteTagNames(noteRef.Guid);
+                Note loadedNote = noteStore.GetNote(noteRef.Guid, true, false, false, false);
+
+                loadedNote.Attributes.ContentClass = null;
+                noteStore.UpdateNote(loadedNote);
+            }
+        }
+
+        private static List<ENSessionFindNotesResult> FindNotes(string evernoteSearchTerm)
+        {
+            List<ENSessionFindNotesResult> readOnlyNotes =
+                EvernoteConnection.CurrentSession.FindNotes(ENNoteSearch.NoteSearch(evernoteSearchTerm),
+                    null,
+                    ENSession.SearchScope.All,
+                    ENSession.SortOrder.Normal,
+                    -1);
+            return readOnlyNotes;
+        }
+    }
+}
